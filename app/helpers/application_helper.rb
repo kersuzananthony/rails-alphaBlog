7711d2899d4b3b2cda32a28b2ac4f1c8327022a9
Kersuzan Anthony <kersuzan.anthony@gmail.com>
2016-04-05 13:44:14 +0800

    Adds markdown feature

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -7,4 +7,25 @@ module ApplicationHelper
     image_tag(gravatar_url, alt: user.username, class: 'img-circle')
   end
 
+  class HTMLwithPygments < Redcarpet::Render::HTML
+    def block_code(code, language)
+      Pygments.highlight(code, lexer: language)
+    end
+  end
+
+  def markdown(content)
+    renderer = HTMLwithPygments.new(hard_wrap: true, filter_html: true)
+    options = {
+      autolink: true,
+      no_intra_emphasis: true,
+      disable_indented_code_blocks: true,
+      fenced_code_blocks: true,
+      lax_spacing: true,
+      superscript: true,
+      strikethrough: true
+    }
+
+    Redcarpet::Markdown.new(renderer, options).render(content).html_safe
+  end
+
 end
